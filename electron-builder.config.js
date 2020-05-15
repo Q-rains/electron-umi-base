9e1c02a91948b06c62f3a4063b1a79186e8c7250
duqiuyu <duqiuyugz@163.com>
2020-05-15 14:49:56 +0800

    修改win打包配置

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -36,8 +36,19 @@ module.exports = {
     target: ['AppImage', 'deb'],
   },
   win: {
-    target: ['squirrel', 'portable'],
+    target: ['nsis', 'portable'],
     icon: 'build/icon.ico', // 客户端图标
+  },
+  nsis: {
+    oneClick: false, // 是否一键安装
+    allowElevation: true,// 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
+    allowToChangeInstallationDirectory: true, // 允许修改安装目录
+    // installerIcon: "./build/icon.ico",// 安装图标
+    // uninstallerIcon: "./build/icons/bbb.ico",//卸载图标
+    // installerHeaderIcon: "./build/icon.ico", // 安装时头部图标
+    createDesktopShortcut: true, // 创建桌面图标
+    createStartMenuShortcut: true,// 创建开始菜单图标
+    shortcutName: 'MINIEYE-DESTOP-APP', // 图标名称
   },
   asar: {
     smartUnpack: true,  // asar打包, 智能提取第三方模块
