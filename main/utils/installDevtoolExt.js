e44c5d9188e7cf8a8c6dc586a6cfdc848a03c007
duqiuyu <duqiuyugz@163.com>
2020-05-19 19:30:38 +0800

    增加devtron工具

--- main/utils/installDevtoolExt.js
+++ main/utils/installDevtoolExt.js
@@ -5,6 +5,8 @@
 const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
 
 module.exports = function() {
+  require('devtron').install()
+
   installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
     .then((name) => console.log(`Added Extension:  ${name}`))
     .catch((err) => console.log('An error occurred: ', err));
